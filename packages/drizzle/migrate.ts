13b011cef0e9c871b7b585424ca46d793b129b0e
Diego Fernandes <diego.schell.f@gmail.com>
2024-03-02 10:31:24 -0300

    ci: Manual vercel build and deploy

--- packages/drizzle/migrate.ts
+++ packages/drizzle/migrate.ts
@@ -1,11 +1,14 @@
 import { neon, neonConfig } from '@neondatabase/serverless'
-import { env } from '@nivo/env'
 import { drizzle } from 'drizzle-orm/neon-http'
 import { migrate } from 'drizzle-orm/neon-http/migrator'
 
 neonConfig.fetchConnectionCache = true
 
-const connection = neon(env.DIRECT_DATABASE_URL)
+if (!process.env.DIRECT_DATABASE_URL) {
+  throw new Error('Invalid environment variable DIRECT_DATABASE_URL')
+}
+
+const connection = neon(process.env.DIRECT_DATABASE_URL)
 const db = drizzle(connection)
 
 migrate(db, { migrationsFolder: __dirname.concat('/migrations') }).then(() => {
