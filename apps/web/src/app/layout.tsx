7bbf7e0280dc85ffa5223369d044a0bb0acb61c0
Victor Nogueira <vmnogueira.ti@gmail.com>
2024-04-25 16:07:05 -0300

    feat(layout): add i18n lang to all pages under app

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,15 +1,19 @@
 import './globals.css'
 
 import { env } from '@nivo/env'
+import { Locale, i18n } from '@nivo/i18n'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ReactNode } from 'react'
 
 import { Toaster } from '@/components/ui/sonner'
 
 import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
+
+export async function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }));
+}
 
 export const metadata: Metadata = {
   metadataBase: new URL(env.NEXT_PUBLIC_VERCEL_URL),
@@ -20,9 +24,15 @@ export const metadata: Metadata = {
   description: 'The all-in-one video solution for online learning.',
 }
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { lang: Locale };
+}) {
   return (
-    <html lang="en" className={inter.variable} suppressHydrationWarning>
+    <html lang={params.lang} className={inter.variable} suppressHydrationWarning>
       <body className="antialiased">
         <Providers>
           {children}
