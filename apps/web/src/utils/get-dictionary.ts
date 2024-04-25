2afd2d3c50bb6bd416945f3c5b12668df2a71aef
Victor Nogueira <vmnogueira.ti@gmail.com>
2024-04-25 16:09:18 -0300

    feat(utils): add get-dictionary function

---
+++ apps/web/src/utils/get-dictionary.ts
@@ -0,0 +1,10 @@
+import "server-only";
+import { Locale } from "@nivo/i18n";
+
+const dictionaries = {
+  en: () => import("../dictionaries/en.json").then((module) => module.default),
+  pt: () => import("../dictionaries/pt.json").then((module) => module.default),
+};
+
+export const getDictionary = async (locale: Locale) =>
+  dictionaries[locale]?.() ?? dictionaries.en();
