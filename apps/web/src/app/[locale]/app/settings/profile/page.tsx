276e1b9991c00ca2e9a4c133bf524b6ccda16a52
Victor Nogueira <vmnogueira.ti@gmail.com>
2024-05-01 17:19:14 -0300

    feat(i18n): use dictionary in settings profile

--- apps/web/src/app/[locale]/app/settings/profile/page.tsx
+++ apps/web/src/app/[locale]/app/settings/profile/page.tsx
@@ -1,82 +1,83 @@
-import { auth } from '@nivo/auth'
-import { Metadata } from 'next'
-import Image from 'next/image'
+import { auth } from '@nivo/auth';
+import Head from 'next/head';
+import Image from 'next/image';
 
-import { Avatar } from '@/components/ui/avatar'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Separator } from '@/components/ui/separator'
+import { Avatar } from '@/components/ui/avatar';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
+import { Locale, getDictionary } from '@nivo/i18n';
 
-export const metadata: Metadata = {
-  title: 'Profile settings',
-}
-
-export default async function ProfilePage() {
-  const session = await auth()
+export default async function ProfilePage({
+  params: { locale },
+}: {
+  params: { locale: Locale }
+}) {
+  const dictionary = await getDictionary(locale);
+  const session = await auth();
 
   if (!session || !session.user) {
-    throw new Error('Invalid session data.')
+    throw new Error(dictionary.error_invalid_session_data || 'Invalid session data.');
   }
 
-  const { user } = session
+  const { user } = session;
 
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Profile</CardTitle>
-        <CardDescription>Update your profile information.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form className="space-y-4">
-          <div className="inline-block space-y-2">
-            <Label>Avatar</Label>
-            <Avatar className="size-16 overflow-visible">
-              {user.image ? (
-                <Image
-                  className="aspect-square size-full rounded-full ring-1 ring-muted"
-                  src={user.image}
-                  width={64}
-                  height={64}
-                  alt=""
-                />
-              ) : (
-                <div className="aspect-square size-full bg-accent" />
-              )}
-            </Avatar>
-          </div>
+    <>
+      <Head>
+        <title>{dictionary.profile_page_title}</title>
+      </Head>
+      <Card>
+        <CardHeader>
+          <CardTitle>{dictionary.profile_title}</CardTitle>
+          <CardDescription>{dictionary.profile_description}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div className="inline-block space-y-2">
+              <Label>{dictionary.profile_label_avatar}</Label>
+              <Avatar className="size-16 overflow-visible">
+                {user.image ? (
+                  <Image
+                    className="aspect-square size-full rounded-full ring-1 ring-muted"
+                    src={user.image}
+                    width={64}
+                    height={64}
+                    alt="User Avatar"
+                  />
+                ) : (
+                  <div className="aspect-square size-full bg-accent" />
+                )}
+              </Avatar>
+            </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              name="name"
-              id="name"
-              defaultValue={user.name ?? ''}
-              disabled
-            />
-          </div>
+            <div className="space-y-2">
+              <Label htmlFor="name">{dictionary.profile_label_name}</Label>
+              <Input
+                name="name"
+                id="name"
+                defaultValue={user.name ?? ''}
+                disabled
+              />
+            </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="email">E-mail</Label>
-            <Input
-              type="email"
-              id="email"
-              defaultValue={user.email ?? ''}
-              disabled
-            />
-          </div>
-          <Separator />
-          <p className="text-[0.8rem] text-muted-foreground">
-            You cannot update your profile yet.
-          </p>
-        </form>
-      </CardContent>
-    </Card>
-  )
+            <div className="space-y-2">
+              <Label htmlFor="email">{dictionary.profile_label_email}</Label>
+              <Input
+                type="email"
+                id="email"
+                defaultValue={user.email ?? ''}
+                disabled
+              />
+            </div>
+            <Separator />
+            <p className="text-[0.8rem] text-muted-foreground">
+              {dictionary.profile_info_update_not_allowed}
+            </p>
+          </form>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
